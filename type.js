ad926f87409cd3175be511f2e3a13b8040598fbe
KaushalSrivastava <46485504+KaushalSrivastava@users.noreply.github.com>
2022-07-20 13:39:24 +0530

    Minor Changes

--- type.js
+++ type.js
@@ -1,3 +1,5 @@
+// DOM objects
+
 const wordinput = document.querySelector('.wordinput');
 const wordcloud = document.querySelector('.wordcloud');
 const scorecount = document.querySelector('.score');
@@ -16,35 +18,46 @@ const hscrdisplay = document.querySelector('.hscrdisplay');
 const husrdisplay = document.querySelector('.husrdisplay');
 const hlvldisplay = document.querySelector('.hlvldisplay');
 
+// Variables Declaration
+
 var randindex = 0;
 var score = 0;
 var missed = -1;
 var correct = 0;
 var level = 5;
 var Uname = 'Anonymous';
-var audiobg = document.querySelector('.audio1');
+var audiobg = document.querySelector('.audio1'); // audio file
+
+// Events
 
 wordinput.addEventListener('input', match);
 clouds.addEventListener('webkitAnimationIteration', changeword);
 clouds.addEventListener('animationiteration', changeword);
 startgame.addEventListener('click', startGameNow);
 
+//Functions
 
+function assignLevel(userlevel){
+    level = userlevel;
+}
 
-slider.oninput = function() {
-    level = this.value / 10;
-  }
+// slider.oninput = function() {
+//     level = this.value / 10;
+//   }
 
 closebtn.onclick = function() {
     resetgame();
   }
 
+
+  //Function to focus on the input box and clear it
 function inpfocus(){
     wordinput.value = '';
-    wordinput.focus();
+    wordinput.focus(); //inbuilt focus function
 
 }
 
+//Function to start the game
 function startGameNow()
 {
     clouds.style.animation = `cloud ${level}s linear infinite forwards`;
@@ -55,11 +68,13 @@ function startGameNow()
     Uname = username.value;
 }
 
+//Function to restart the game
 function resetgame()
 {
-    window.location.reload();
+    window.location.reload();  //reloading the window
 }
 
+//Function to change the word when cloud animation ends
 function changeword()
 {
     checker();
@@ -77,16 +92,18 @@ function changeword()
     }
   
 }
-
+//Function to Set the Highscore and retrieve the High Score
 function sethighscore(score, Uname, level){
-    var hscore = window.localStorage.getItem('Score');
+    var hscore = window.localStorage.getItem('Score'); //Getting score from Local Storage
 
     scrdisplay.innerHTML = `${score}`;
     usrdisplay.innerHTML = `${Uname}`;
     lvldisplay.innerHTML = `${level}`;
     
-    if(hscore <= score)
+    if(hscore <= score) //comparing previous high score with current score
     {
+        //setting current details in high score if previous highscore is less
+        //than the current score
         window.localStorage.setItem('Username', Uname);
         window.localStorage.setItem('Score', score);
         window.localStorage.setItem('Level', level);
@@ -101,13 +118,14 @@ function sethighscore(score, Uname, level){
     popbox.style.display = 'block';
 }
 
+//Function to change word's look and increment score when words are typed correctly
 function markcorrect()
 {
    wordcloud.style.color = 'grey';
    correct = 1;
 }
 
-
+//Function to increment the missed count when words are typed wrong
 function checker()
 {
      if(correct === 0)
@@ -117,7 +135,8 @@ function checker()
      }
  
 }
-
+//Function that runs each time when user inputs a character and checks if the user
+// input is matching the word in the cloud
 function match(){
    if(wordinput.value == wordcloud.innerHTML) 
    { 
